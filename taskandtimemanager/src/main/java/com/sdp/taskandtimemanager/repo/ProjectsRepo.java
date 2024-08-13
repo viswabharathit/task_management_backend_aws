6f5475d16a8524c30ca29639dc50e38af5d5bc91
VISWABHARATHI T <125241243+viswabharathit@users.noreply.github.com>
2024-08-13 16:29:48 +0530

    Delete taskandtimemanager directory

--- taskandtimemanager/src/main/java/com/sdp/taskandtimemanager/repo/ProjectsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sdp.taskandtimemanager.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sdp.taskandtimemanager.model.Projects;
-
-public interface ProjectsRepo extends JpaRepository<Projects, Long> {
-
-}
